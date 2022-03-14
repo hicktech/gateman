17b6a9a49aba4a773797b5dd2e14485680816994
John Wass <jwass3@gmail.com>
2022-03-14 13:28:53 -0400

    example: read encoder

---
+++ README.md
@@ -0,0 +1,14 @@
+gateman firmware
+===
+
+
+## PWM config
+
+Boot config needs an entry to configure GPIO (BCM) 12 as PWM0
+
+`dtoverlay=pwm,pin=12,func=4`
+
+
+## License
+
+GPL v3
---
+++ examples/read_encoder.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+
+use rppal::gpio::Level::*;
+use rppal::gpio::{Gpio, Level};
+
+const CLOCK_PIN: u8 = 23;
+const DATA_PIN: u8 = 24;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let clock = Gpio::new()?.get(CLOCK_PIN)?.into_input_pullup();
+    let data = Gpio::new()?.get(DATA_PIN)?.into_input_pullup();
+
+    let mut state: u16 = 0;
+    let mut encoder_idx: i64 = 0;
+
+    loop {
+        let c = clock.read() as u16;
+        let d = data.read() as Level;
+
+        state = (&state << 1) | c | 0xe000;
+        if state == 0xf000 {
+            match d {
+                High => encoder_idx += 1,
+                Low => encoder_idx -= 1,
+            }
+
+            state = 0;
+            println!("idx {}", encoder_idx);
+        }
+    }
+}
