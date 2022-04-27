hicktech/gateman

commit c569328cd246c05c52148238e50ca57e7b12e8b0

Flip level for direction reversal

src/drive.rs
@@ -183,8 +183,8 @@ enum Direction {
 impl From<Direction> for Level {
     fn from(d: Direction) -> Self {
         match d {
-            Open => Low,
-            Close => High,
+            Open => High,
+            Close => Low,
         }
     }
 }

commit 02e7aff604029f19acaac64028b018bab9431631

Flip direction to match level
Fix regression caused by partial change in c569328c

src/drive.rs
@@ -203,8 +203,8 @@ impl From<Direction> for Level {
 impl From<Level> for Direction {
     fn from(l: Level) -> Self {
         match l {
-            Low => Open,
-            High => Close,
+            High => Open,
+            Low => Close,
         }
     }
 }